99eff19f4826fec85774eb0713cfcc3c710647a2
Honzitko <236162629+honziitko@users.noreply.github.com>
2025-11-08 10:15:27 +0100

    [Zig] Add build options

--- zig/build.zig
+++ zig/build.zig
@@ -1,8 +1,24 @@
 const std = @import("std");
 
+const default_states = 6;
+const default_symbols = 2;
+// Chosen by a fair dice roll.
+// Guranteed to be random.
+const default_seed = 4;
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
+    const num_states = b.option(usize, "states", "How many states (excluding halt) does the TM have") orelse default_states;
+    const num_symbols = b.option(usize, "symbols", "How many symbols does the tape have") orelse default_symbols;
+    const seed = b.option(u64, "seed", "The initial seed") orelse default_seed;
+    if (num_states == 0) {
+        std.debug.panic("There must be at least one state\n", .{});
+    }
+    if (num_symbols == 0) {
+        std.debug.panic("There must be at least one symbol\n", .{});
+    }
 
     const tm_module = b.addModule("tm", .{
         .root_source_file = b.path("src/tm.zig"),
@@ -24,10 +40,8 @@ pub fn build(b: *std.Build) void {
     b.getInstallStep().dependOn(&b.addInstallArtifact(exe, .{}).step);
 
     const options = b.addOptions();
-    options.addOption(usize, "num_states", 10);
-    options.addOption(usize, "num_symbols", 2);
-    // Chosen by a fair dice roll.
-    // Guranteed to be random.
-    options.addOption(u64, "seed", 4);
+    options.addOption(usize, "num_states", num_states);
+    options.addOption(usize, "num_symbols", num_symbols);
+    options.addOption(u64, "seed", seed);
     glc_module.addOptions("config", options);
 }
